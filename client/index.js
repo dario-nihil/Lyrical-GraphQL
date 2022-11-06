80e08667a92ef1ff4f6734e3c5a622e48816ddee
dario-nihil <dario.nihil@gmail.com>
2022-11-06 15:11:17 +0100

    installed nodemon and updated packages

--- client/index.js
+++ client/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return <div>Lyrical</div>;
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Root />);
+
+// ReactDOM.render(<Root />, document.querySelector("#root"));
